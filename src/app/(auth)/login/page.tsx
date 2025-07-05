997283c30a5e7623e5d2d1b164c510bb8d7e1f46
tofayeLL <ahmedtofayel132@gmail.com>
2025-07-05 23:08:47 +0600

    finished and deploy code

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,12 @@
 // import LoginForm from '@/components/home/LoginForm';
-import LoginForm from '@/components/home/LoginForm';
+
 import React from 'react';
 
 const page = () => {
     return (
         <section>
        
-            <LoginForm switchToSignup={() => { }} />
+          
             
         </section>
     );
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,10 +1,10 @@
-import SignupForm from '@/components/home/SignupForm';
+
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-           <SignupForm switchToLogin={() => {}} /> 
+          
         </div>
     );
 };
